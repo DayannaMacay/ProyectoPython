01a47290265e8b7e3342b0e51c8d824b1ca8e0cc
DayannaMacay <dayanamacay@hotmail.com>
2024-01-07 00:09:38 -0500

    Conexión entre frontend y backend

---
+++ urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for ProyectoFinal project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views as core
+from django.conf import settings
+
+urlpatterns = [
+    path('', core.home, name="home"),
+    path('about/', core.about, name="about"),
+    path('service/', core.service, name="service"),
+    path('contact/', core.contact, name="contact"),
+    path('admin/', admin.site.urls),
+]
